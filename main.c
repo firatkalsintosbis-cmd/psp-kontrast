c50e8ba9149a9b1ebf12ba082237bb0edcd40b5d
firatkalsintosbis-cmd <firatkalsintosbis@gmail.com>
2026-05-13 11:28:07 +0300

    main.c

--- main.c
+++ main.c
@@ -7,40 +7,64 @@
 PSP_MODULE_INFO("MinecraftPE", 0, 1, 1);
 PSP_MAIN_THREAD_ATTR(PSP_THREAD_ATTR_USER);
 
-// Mobil tarzdaki o geniş görüş açısını ayarlıyoruz (FOV)
-float fov = 75.0f; 
+// Grafik listesi için bellek ayırıyoruz
+static unsigned int __attribute__((aligned(16))) list[262144];
 
-void setupGu() {
+// Mobil tarzı Hotbar çizimi (Kareleri ekrana basar)
+void drawHotbar(int selected) {
+    int i;
+    for(i = 0; i < 9; i++) {
+        int x = 150 + (i * 21); // Hotbar'ı ortalıyoruz
+        int y = 240;
+        
+        unsigned int color = (i == selected) ? 0xFFFFFFFF : 0xFF888888; 
+        
+        // PSP'nin grafik işlemcisine bir dikdörtgen çiz komutu veriyoruz
+        sceGuColor(color);
+        // Basit bir kutu çizimi (Gerçek texture gelene kadar yer tutucu)
+        struct { float x, y, z; }* vertices = sceGuGetMemory(2 * sizeof(*vertices));
+        vertices[0].x = x;     vertices[0].y = y;     vertices[0].z = 0;
+        vertices[1].x = x+20;  vertices[1].y = y+20;  vertices[1].z = 0;
+        sceGuDrawArray(GU_SPRITES, GU_VERTEX_32BITF|GU_TRANSFORM_2D, 2, 0, vertices);
+    }
+}
+
+int main() {
     sceGuInit();
     sceGuStart(GU_DIRECT, list);
     sceGuDrawBuffer(GU_PSM_8888, (void*)0, 512);
     sceGuDispBuffer(480, 272, (void*)0x88000, 512);
-    sceGuClearColor(0xFFD8A05E); // Mobil Minecraft gökyüzü rengi (Açık Mavi)
+    sceGuClearColor(0xFFD8A05E); // PE Mavi Gökyüzü
     sceGuFinish();
+    sceGuSync(0,0);
+    sceDisplayWaitVblankStart();
     sceGuDisplay(GU_TRUE);
-}
 
-int main() {
-    setupGu();
     SceCtrlData pad;
+    int selectedSlot = 0;
 
     while(1) {
         sceCtrlReadBufferPositive(&pad, 1);
         
+        // L ve R ile slot değiştirme
+        if(pad.Buttons & PSP_CTRL_RTRIGGER) {
+            selectedSlot = (selectedSlot + 1) % 9;
+            sceKernelDelayThread(200000); 
+        }
+        if(pad.Buttons & PSP_CTRL_LTRIGGER) {
+            selectedSlot = (selectedSlot - 1 + 9) % 9;
+            sceKernelDelayThread(200000);
+        }
+
         sceGuStart(GU_DIRECT, list);
         sceGuClear(GU_COLOR_BUFFER_BIT);
-        
-        // Buraya blok çizim kodları gelecek
-        
+
+        drawHotbar(selectedSlot); // Hotbar'ı her karede çiz
+
         sceGuFinish();
         sceGuSync(0,0);
         sceDisplayWaitVblankStart();
         sceGuSwapBuffers();
-        
-        if(pad.Buttons & PSP_CTRL_HOME) break;
     }
-
-    sceKernelExitGame();
     return 0;
 }
-
